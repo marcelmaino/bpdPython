89434a2807186f06ce1fc0463e23d2bdb9bea863
user <user@example.com>
2025-07-17 10:56:32 -0300

    refactor: Atualiza script do Google Analytics para versão mais moderna

--- utils.py
+++ utils.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 def insert_google_analytics():
     st.markdown(
-        f"""
+        '''
         <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DY8SSHYRR1"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){{dataLayer.push(arguments);}}
-          gtag('js', new Date());
-          gtag('config', 'G-DY8SSHYRR1');
-        </script>
-        """,
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DY8SSHYRR1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-DY8SSHYRR1');
+</script>
+        ''',
         unsafe_allow_html=True
     )
